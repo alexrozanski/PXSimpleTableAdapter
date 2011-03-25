9aa633b7cb46b30fb701f16bb43596fed94ddb51
Alex Rozanski <alex@rozanski.com>
2011-03-25 10:50:16 +0000

    Add a viewController property on PXSimpleTableRow

--- Classes/PXSimpleTableRow.h
+++ Classes/PXSimpleTableRow.h
@@ -12,15 +12,19 @@
 @interface PXSimpleTableRow : NSObject {
     NSString *_title;
     UIImage *_icon;
+    UIViewController *_viewController;
 }
 
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, retain) UIImage *icon;
+@property (nonatomic, retain) UIViewController *viewController;
 
 + (id)rowWithTitle:(NSString*)title;
 + (id)rowWithTitle:(NSString*)title icon:(UIImage*)icon;
++ (id)rowWithTitle:(NSString *)title icon:(UIImage*)icon viewController:(UIViewController*)controller;
 
 - (id)initWithTitle:(NSString*)title;
 - (id)initWithTitle:(NSString*)title icon:(UIImage*)icon;
+- (id)initWithTitle:(NSString *)title icon:(UIImage*)icon viewController:(UIViewController*)controller;
 
 @end
--- Classes/PXSimpleTableRow.m
+++ Classes/PXSimpleTableRow.m
@@ -13,32 +13,53 @@
 
 @synthesize title = _title;
 @synthesize icon = _icon;
+@synthesize viewController = _viewController;
 
 + (id)rowWithTitle:(NSString*)title
 {
-    return [self rowWithTitle:title icon:nil];
+    return [self rowWithTitle:title icon:nil viewController:nil];
 }
 
 + (id)rowWithTitle:(NSString*)title icon:(UIImage*)icon
 {
-    return [[[self alloc] initWithTitle:title icon:icon] autorelease];
+    return [self rowWithTitle:title icon:icon viewController:nil];
+}
+
++ (id)rowWithTitle:(NSString *)title icon:(UIImage*)icon viewController:(UIViewController*)controller
+{
+    return [[[self alloc] initWithTitle:title icon:icon viewController:controller] autorelease];
 }
 
 #pragma mark - Init/Dealloc
 
 - (id)initWithTitle:(NSString*)title
 {
-    return [self initWithTitle:title icon:nil];
+    return [self initWithTitle:title icon:nil viewController:nil];
 }
 
 - (id)initWithTitle:(NSString*)title icon:(UIImage*)icon
 {
+    return [self initWithTitle:title icon:icon viewController:nil];
+}
+
+- (id)initWithTitle:(NSString*)title icon:(UIImage*)icon viewController:(UIViewController*)controller
+{
     if((self = [super init])) {
         _title = [title copy];
         _icon = [icon retain];
+        _viewController = [controller retain];
     }
     
     return self;
+}
+
+- (void)dealloc
+{
+    [_title release], _title=nil;
+    [_icon release], _icon=nil;
+    [_viewController release], _viewController=nil;
+    
+    [super dealloc];
 }
 
 @end
